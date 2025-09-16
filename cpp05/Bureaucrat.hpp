043fe4c6625696b696aec76b204e188ffc8f8b03
Teo Montani <tmontani@c1r6s4.42lausanne.ch>
2025-09-16 15:13:21 +0200

    start cpp05

---
+++ cpp05/Bureaucrat.hpp
@@ -0,0 +1,19 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+#include <iostream>
+
+// bureaucrat name
+// grade
+// grade error throws exception too high too low
+
+class Bureaucrat{
+    private:
+
+    int grade;
+    std:: string name;
+    
+
+}
+
+
+#endif
