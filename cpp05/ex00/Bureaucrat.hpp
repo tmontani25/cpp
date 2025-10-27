410b61e77e31a546cb68d7bd2acf03c38e94894f
tmontani25 <teomontani27@gmail.com>
2025-10-27 18:25:50 +0100

    cpp05 start

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+//class bureaucrat simple avec un grade
+// membres qui permette de verifier le grade
+
+class bureaucrat {
+    private:
+        int grade;
+    public:
+        bureaucrat(int grd) : grade(grd){}
+
+        grade_too_high
+};
